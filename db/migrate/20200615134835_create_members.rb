ad1c9c58bf97f4df3fd424a9939a63b41ddac138
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2020-06-15 20:54:16 +0700

    Initial Commit
    Initial Commit

---
+++ db/migrate/20200615134835_create_members.rb
@@ -0,0 +1,16 @@
+class CreateMembers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :members do |t|
+      t.string :name
+      t.string :phone
+      t.string :address
+      t.string :cetya
+      t.string :gender
+      t.string :education
+      t.date :date_of_birth
+      t.string :place_of_birth
+
+      t.timestamps
+    end
+  end
+end
