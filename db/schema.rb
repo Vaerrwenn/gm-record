299c6d575e77658d90deb5b69ab8287d592f9b63
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2020-06-17 21:10:49 +0700

    Added Member Index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_131751) do
+ActiveRecord::Schema.define(version: 2020_06_15_134835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,19 @@ ActiveRecord::Schema.define(version: 2020_06_15_131751) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.string "cetya"
+    t.string "gender"
+    t.string "education"
+    t.date "date_of_birth"
+    t.string "place_of_birth"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
